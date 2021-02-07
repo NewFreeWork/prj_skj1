10e0343885b3e84ec82196fc846948bd5fcdb028
khlee <kihunopha@gmail.com>
2021-02-07 20:46:15 +0900

    예제사이트 폴더 추가

--- proj_skj1_django/proj_skj1_django/urls.py
+++ proj_skj1_django/proj_skj1_django/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#]
 
 #khlee add 21/02/01
 from django.urls import include, path
@@ -34,6 +33,7 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
 ]
 
